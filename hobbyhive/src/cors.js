fa0d0fedbd91e277099e8f1cd686191ca4a9bde5
Bianca Onea <bianca.onea100@gmail.com>
2023-06-14 15:05:14 +0200

    added cors file for vercel

---
+++ hobbyhive/src/cors.js
@@ -0,0 +1,14 @@
+module.exports = (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Handle the request
+  // ...
+
+  // Return the response
+  res.send("Hello from Vercel!");
+};
